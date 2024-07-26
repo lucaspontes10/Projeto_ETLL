210f7076b7245680a57121c836c72d7f25c8058b
lucaspontes10 <lucaspontesaraujo8@gmail.com>
2024-07-26 10:39:12 -0300

    a

--- python/aula44.py
+++
@@ -1,5 +0,0 @@
-nome = 'lucas'
-preço = 1000.95897643
-variavel = '%s, o preço é R$%.2f ' % (nome, preço)
-print(variavel)
-print('o hexadecimal de %d é %08x' % (1500,1500))
--- python/novo.py
+++
@@ -1,7 +0,0 @@
-nome = input('digite seu nome: ')
-encontrar =  input ('digite oque deseja encontrar:')
-
-if encontrar in nome:
-    print(f'{encontrar} esta em {nome}')
-else:
-    print(f'{encontrar} não esta em {nome}')
